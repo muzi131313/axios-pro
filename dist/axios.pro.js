7491acf8d1c1fd1a736b4ec201c10724ef90ea11
muzi131313 <muzi131313@163.com>
2019-06-13 17:01:35 +0800

    rebuild

--- dist/axios.pro.js
+++ dist/axios.pro.js
@@ -2513,10 +2513,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  * @createTime 2019年02月21日16:08:38
  */
 var combine = function combine() {
-  for (var _len = arguments.length, more = Array(_len), _key = 0; _key < _len; _key++) {
-    more[_key] = arguments[_key];
-  }
-
+  var more = Array.prototype.slice.call(arguments);
   if (!more) {
     return console.warn('combine() require at least one params');
   }
@@ -7657,18 +7654,18 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 // var stringifyTypes = ['post', 'put', 'patch', 'delete']
 
-module.exports = function (instance, config) {
+module.exports = function (instance, baseConfig) {
   // request 拦截器
   instance.interceptors.request.use(function (config) {
     // Tip: 1
     // 请求开始的时候可以结合 vuex 开启全屏的 loading 动画
-    if (config.handlers && config.handlers.loading && config.handlers.loading.open) {
-      config.handlers && config.handlers.loading && config.handlers.loading.start && config.handlers.loading.start();
+    if (baseConfig.handlers && baseConfig.handlers.loading && baseConfig.handlers.loading.open) {
+      baseConfig.handlers && baseConfig.handlers.loading && baseConfig.handlers.loading.start && baseConfig.handlers.loading.start();
     }
 
     // Tip: 2
     // 带上 token , 可以结合 vuex 或者重 localStorage
-    config.handlers && config.handlers.config && config.handlers.config(config);
+    baseConfig.handlers && baseConfig.handlers.config && baseConfig.handlers.config(config);
 
     // Tip: 3
     // TODO: 根据请求方法，序列化传来的参数，根据后端需求是否序列化
@@ -7691,7 +7688,7 @@ module.exports = function (instance, config) {
     if (error.code === 'ECONNABORTED' && error.message.indexOf('timeout') !== -1) {
       // TODO: 超时处理
       console.log('根据你设置的timeout/真的请求超时 判断请求现在超时了，你可以在这里加入超时的处理方案');
-      config.handlers && config.handlers.timeout && config.handlers.timeout(error.message);
+      baseConfig.handlers && baseConfig.handlers.timeout && baseConfig.handlers.timeout(error.message);
       // return service.request(originalRequest);//例如再重复请求一次
     }
     //  2.需要重定向到错误页面
