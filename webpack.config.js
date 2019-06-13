745c70a4bed003642fc23e69b1c14a91992da93a
muzi131313 <muzi131313@163.com>
2019-06-13 16:06:42 +0800

    更改为umd

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const path = require('path')
 
 module.exports = {
   entry: {
@@ -12,10 +13,13 @@ module.exports = {
   output: {
     filename: '[name].js',
     libraryExport: 'default',
-    library: 'axiosPro',
-    libraryTarget: 'commonjs2'
+    library: 'axiosPro', // 指定类库名,主要用于直接引用的方式(比如使用script 标签)
+    libraryTarget: 'umd', // 定义打包方式Universal Module Definition,同时支持在CommonJS、AMD和全局变量使用
+    globalObject: 'this', // 定义全局变量,兼容node和浏览器运行，避免出现"window is not defined"的情况
+    umdNamedDefine: true
   },
-  devtool: 'source-map',
+  mode: 'production', // 告诉webpack使用production模式的内置优化,
+  devtool: 'source-map', // 开启sourceMap，方便调试
   optimization: {
     minimize: true,
     minimizer: [
@@ -38,6 +42,9 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
         exclude: [ /node_modules/ ]
       }
     ]
