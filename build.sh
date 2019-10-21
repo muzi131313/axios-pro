e5bd865c6521758af410dd4a7dc95648f710e907
muzi131313 <muzi131313@163.com>
2019-10-21 17:48:24 +0800

    add travis ci

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 提交生成的文件
+git add .
+git commit -m 'deploy'
+
+git push -f "https://${access_token}@github.com/muzi131313/axios-pro.git" master:master
+
+cd -
