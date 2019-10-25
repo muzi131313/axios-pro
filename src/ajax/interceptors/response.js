92a7d3053a42fba2eccbb671d09a97556f386b53
muzi131313 <muzi131313@163.com>
2019-10-25 14:03:49 +0800

    fix: lib may be used in browser, cannot use process

--- src/ajax/interceptors/response.js
+++ src/ajax/interceptors/response.js
@@ -4,7 +4,6 @@
  * @createTime 2018年11月03日23:16:47
  */
 var getLanguage = require('../../language/index')
-var { IS_TEST } = require('../../constant')
 
 module.exports = function (instance, config) {
   var objType = function (obj) {
@@ -57,57 +56,8 @@ module.exports = function (instance, config) {
         var language = getLanguage(config.handlers && config.handlers.language)
         var errMessage = language.msg[err.response.status] || 'unknow error'
         err.message = errMessage
-        // switch (err.response.status) {
-        //   case 400:
-        //     err.message = '请求错误'
-        //     break
-
-        //   case 401:
-        //     err.message = '未授权，请登录'
-        //     break
-
-        //   case 403:
-        //     err.message = '拒绝访问'
-        //     break
-
-        //   case 404:
-        //     err.message = '请求地址出错: ' + err.response.config.url
-        //     break
-
-        //   case 408:
-        //     err.message = '请求超时'
-        //     break
-
-        //   case 500:
-        //     err.message = '服务器内部错误'
-        //     break
-
-        //   case 501:
-        //     err.message = '服务未实现'
-        //     break
-
-        //   case 502:
-        //     err.message = '网关错误'
-        //     break
-
-        //   case 503:
-        //     err.message = '服务不可用'
-        //     break
-
-        //   case 504:
-        //     err.message = '网关超时'
-        //     break
-
-        //   case 505:
-        //     err.message = 'HTTP版本不受支持'
-        //     break
-
-        //   default:
-        // }
       }
-      if (!IS_TEST()) {
-        console.error('catch error', err)
-      }
+      // console.error('catch error', err)
 
       if (config.handlers && config.handlers.error) {
         config.handlers.error(err)
--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,2 @@
 export var EN_TYPE = 'en'
 export var CN_TYPE = 'cn'
-export var IS_TEST = function() {
-  return process.env.NODE_ENV === 'test'
-}
