e5313d6715e661b489d7973e9c49c30cd3ba3dd5
muzi131313 <muzi131313@163.com>
2019-11-07 14:40:14 +0800

    style: remove unused code

--- src/ajax/interceptors/request.js
+++ src/ajax/interceptors/request.js
@@ -21,16 +21,6 @@ module.exports = function (instance, baseConfig) {
       // 带上 token , 可以结合 vuex 或者重 localStorage
       baseConfig.handlers && baseConfig.handlers.config && baseConfig.handlers.config(config)
 
-      // Tip: 3
-      // TODO: 根据请求方法，序列化传来的参数，根据后端需求是否序列化
-      // TODO: 这里加上这个后, post请求携带的数据丢失
-      // var method = config.method
-      // var params = config.params
-      // method = method.toLocaleLowerCase()
-      // if (~stringifyTypes.indexOf(method)) {
-      //   config.data = qs.stringify(params)
-      //   delete config.params
-      // }
       return config
     },
     function (error) {
@@ -44,20 +34,17 @@ module.exports = function (instance, baseConfig) {
         && error.message.indexOf('timeout') !== -1) {
         // TODO: 超时处理
         console.log('根据你设置的timeout/真的请求超时 判断请求现在超时了，你可以在这里加入超时的处理方案')
-        baseConfig.handlers && baseConfig.handlers.timeout && baseConfig.handlers.timeout(error.message)
+        var hasOwnHandleTimeout = baseConfig.handlers && baseConfig.handlers.timeout
+        hasOwnHandleTimeout && baseConfig.handlers.timeout(error.message)
         // return service.request(originalRequest);//例如再重复请求一次
-        return Promise.resolve(error)
+        return hasOwnHandleTimeout
+          ? Promise.resolve(error)
+          : Promise.reject(error)
       }
       //  2.需要重定向到错误页面
-      // const errorInfo = error.response
-      // // console.log(errorInfo);
-      // if (errorInfo) {
-      //   // error =errorInfo.data//页面那边catch的时候就能拿到详细的错误信息,看最下边的Promise.reject
-      //   const errorStatus = errorInfo.status // 404 403 500 ... 等
-      //   router.push({
-      //     path: `/error/${errorStatus}`
-      //   })
-      // }
+      // const errorInfo = error.response || {}
+      // const errorStatus = errorInfo.status
+
       return Promise.reject(error) // 在调用的那边可以拿到(catch)你想返回的错误信息
     }
   )
